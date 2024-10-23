14f0fa5e507cfd6283ba7a35d21ea7462e73ce30
Fiodaru <f.perakhozhau@pvp.co.jp>
2024-10-23 15:09:36 +0900

    Add UnitAnimation script
    Only implement riffle idle/riffle run animations

---
+++ Assets/Scripts/UnitAnimation.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class UnitAnimation : MonoBehaviour
+{
+    public const string IS_WALKING = "IsWalking";
+    [SerializeField] private Unit unit;
+    [SerializeField] private Animator animator;
+
+    private void Update()
+    {
+        animator.SetBool(IS_WALKING, unit.IsWalking);
+    }
+}
