3965f2960f92f8faaed1e3933a014a0650cfe92a
Fiodaru <f.perakhozhau@pvp.co.jp>
2024-10-23 15:45:07 +0900

    Convert Unit Action System into Singleton
    Fire event once we change selected Unit, to trigger unit selected visual

--- Assets/Scripts/UnitActionSystem.cs
+++ Assets/Scripts/UnitActionSystem.cs
@@ -1,9 +1,26 @@
+using System;
 using UnityEngine;
 
 public class UnitActionSystem : MonoBehaviour
 {
+    public static UnitActionSystem Instance { get; private set; }
+
+    public event EventHandler OnSelectedUnitChanged;
+
     [SerializeField] private Unit selectedUnit;
     [SerializeField] private LayerMask unitLayerMask;
+
+    private void Awake()
+    {
+        if(Instance != null)
+        {
+            Debug.Log("There is more than 1 UnitActionSystem! " + transform + " - " + Instance);
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+    }
 
     private void Update()
     {
@@ -22,12 +39,24 @@ public class UnitActionSystem : MonoBehaviour
         {
             if(hitInfo.collider.TryGetComponent<Unit>(out Unit unit))
             {
-                selectedUnit = unit;
+                SetSelectedUnit(unit);
 
                 return true;
             }
         }
 
         return false;
+    }
+
+    private void SetSelectedUnit(Unit unit)
+    {
+        selectedUnit = unit;
+
+        OnSelectedUnitChanged?.Invoke(this, EventArgs.Empty);
+    }
+
+    public Unit GetSelectedUnit() 
+    { 
+        return selectedUnit; 
     }
 }
