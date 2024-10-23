1b23a0877728f4a8e67fe20b2d58c09a0ffccb35
Fiodaru <f.perakhozhau@pvp.co.jp>
2024-10-23 15:26:05 +0900

    Add UnitActionSystem
    Handles different Unit Actions. Handles selection of the Unit and its movement.

---
+++ Assets/Scripts/UnitActionSystem.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class UnitActionSystem : MonoBehaviour
+{
+    [SerializeField] private Unit selectedUnit;
+    [SerializeField] private LayerMask unitLayerMask;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (TryHandleUnitSelection()) return;
+
+            selectedUnit.Move(MouseWorld.GetPosition());
+        }
+    }
+
+    private bool TryHandleUnitSelection()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if(Physics.Raycast(ray, out RaycastHit hitInfo, float.MaxValue, unitLayerMask))
+        {
+            if(hitInfo.collider.TryGetComponent<Unit>(out Unit unit))
+            {
+                selectedUnit = unit;
+
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
