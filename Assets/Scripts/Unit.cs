80737dd36fb3e32e6b0081b59301aa17a21d68e7
Fiodaru <f.perakhozhau@pvp.co.jp>
2024-10-24 17:52:15 +0900

    Update Unit information in GridObjects

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -3,12 +3,19 @@ using UnityEngine;
 public class Unit : MonoBehaviour
 {
     private Vector3 targetPosition;
+    private GridPosition gridPosition;
 
     public bool IsWalking { get; private set; }
 
     private void Awake()
     {
         targetPosition = transform.position;
+    }
+
+    private void Start()
+    {
+        gridPosition = LevelGrid.Instance.GetGridPosition(transform.position);
+        LevelGrid.Instance.AddUnitAtGridPosition(this, gridPosition);
     }
 
     private void Update()
@@ -28,6 +35,14 @@ public class Unit : MonoBehaviour
         {
             IsWalking=false;
         }
+
+        GridPosition newGridPosition = LevelGrid.Instance.GetGridPosition(transform.position);
+        if(newGridPosition != gridPosition)
+        {
+            LevelGrid.Instance.MoveUnitToGridPosition(this, gridPosition, newGridPosition);
+            gridPosition = newGridPosition;
+        }
+
     }
 
     public void Move(Vector3 targetPosition)
