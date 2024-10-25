50f67f8982fdcaf66eb052ab30153ff978e54a76
Fiodaru <f.perakhozhau@pvp.co.jp>
2024-10-25 11:41:29 +0900

    Extract Move functionality to Move Action script

---
+++ Assets/Scripts/MoveAction.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class MoveAction : MonoBehaviour
+{
+    private Vector3 targetPosition;
+
+    public bool IsWalking { get; private set; }
+
+    private void Awake()
+    {
+        targetPosition = transform.position;
+    }
+
+    private void Update()
+    {
+        float stoppingDistance = 0.1f;
+        if (Vector3.Distance(targetPosition, transform.position) > stoppingDistance)
+        {
+            Vector3 moveDirection = (targetPosition - transform.position).normalized;
+            float moveSpeed = 4f;
+            transform.position += moveDirection * moveSpeed * Time.deltaTime;
+            IsWalking = true;
+
+            float rotateSpeed = 10f;
+            transform.forward = Vector3.Lerp(transform.forward, moveDirection, rotateSpeed * Time.deltaTime);
+        }
+        else
+        {
+            IsWalking = false;
+        }
+    }
+
+    public void Move(Vector3 targetPosition)
+    {
+        this.targetPosition = targetPosition;
+    }
+}
