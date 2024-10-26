37103efd8e5cf356050da6c21a5b25778031b6c1
Fiodaru <f.perakhozhau@pvp.co.jp>
2024-10-26 14:27:30 +0900

    Add Move Validation to MoveAction
    Also updated Move function to accept GridPosition instead of Vector3

--- Assets/Scripts/MoveAction.cs
+++ Assets/Scripts/MoveAction.cs
@@ -1,7 +1,11 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class MoveAction : MonoBehaviour
 {
+    [SerializeField] private Unit unit;
+    [SerializeField] private int maxMoveDistance = 2;
+
     private Vector3 targetPosition;
 
     public bool IsWalking { get; private set; }
@@ -30,8 +34,51 @@ public class MoveAction : MonoBehaviour
         }
     }
 
-    public void Move(Vector3 targetPosition)
+    public bool IsValidActionGridPosition(GridPosition gridPosition)
     {
-        this.targetPosition = targetPosition;
+        List<GridPosition> validActionGridPositionList = GetValidActionGridPositionList();
+        return validActionGridPositionList.Contains(gridPosition);
+    }
+
+    public List<GridPosition> GetValidActionGridPositionList()
+    {
+        List<GridPosition> validActionGridPositionList = new List<GridPosition>();
+
+        GridPosition unitGridPosition = unit.GetGridPosition();
+        for (int x = -maxMoveDistance; x <= maxMoveDistance; x++)
+        {
+            for (int z = -maxMoveDistance; z <= maxMoveDistance; z++)
+            {
+                GridPosition offsetGridPosition = new GridPosition(x, z);
+                GridPosition testGridPosition = offsetGridPosition + unitGridPosition;
+
+                if(!LevelGrid.Instance.IsValidGridPosition(testGridPosition))
+                {
+                    //Not valid GridPosition
+                    continue;
+                }
+
+                if(testGridPosition == unitGridPosition)
+                {
+                    //Unit is already located in that GridPosition
+                    continue;
+                }
+
+                if (LevelGrid.Instance.HasAnyUnitOnGridPosition(testGridPosition))
+                {
+                    //GridPosition is already occupied by another unit
+                    continue;
+                }
+
+                validActionGridPositionList.Add(testGridPosition);
+            }
+        }
+
+        return validActionGridPositionList;
+    }
+
+    public void Move(GridPosition gridPosition)
+    {
+        targetPosition = LevelGrid.Instance.GetWorldPosition(gridPosition);
     }
 }
