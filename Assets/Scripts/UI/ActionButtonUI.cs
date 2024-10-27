62f5ff8c79e496f6489a348c833f7a2d534984e8
Fiodaru <f.perakhozhau@pvp.co.jp>
2024-10-27 15:37:40 +0900

    Hook Action Selection to Action Button click

--- Assets/Scripts/UI/ActionButtonUI.cs
+++ Assets/Scripts/UI/ActionButtonUI.cs
@@ -1,11 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ActionButtonUI : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI textMeshPro;
+    [SerializeField] private Button button;
+
     private BaseAction baseAction;
 
     public void SetBaseAction(BaseAction baseAction)
@@ -13,5 +14,9 @@ public class ActionButtonUI : MonoBehaviour
         this.baseAction = baseAction;
 
         textMeshPro.text = baseAction.GetActionName().ToUpper();
+        button.onClick.AddListener(() =>
+        {
+            UnitActionSystem.Instance.SetSelectedAction(baseAction);
+        });
     }
 }
