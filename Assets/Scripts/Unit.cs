07326b623510733901f62b39d4270f0dbacaecec
Fiodaru <f.perakhozhau@pvp.co.jp>
2024-11-05 15:22:43 +0900

    Update GridSystemVisual when Unit moves.
    Fire event when unit changes grid position, which is used to update GridSystemVisual

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -41,8 +41,9 @@ public class Unit : MonoBehaviour
         GridPosition newGridPosition = LevelGrid.Instance.GetGridPosition(transform.position);
         if(newGridPosition != gridPosition)
         {
-            LevelGrid.Instance.UnitMovedToGridPosition(this, gridPosition, newGridPosition);
+            GridPosition oldGridPosition = gridPosition;
             gridPosition = newGridPosition;
+            LevelGrid.Instance.UnitMovedToGridPosition(this, oldGridPosition, newGridPosition);
         }
     }
 
