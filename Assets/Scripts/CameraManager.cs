b0e6e3d97428953c8cd3370440683f82e6d08333
Fiodaru <f.perakhozhau@pvp.co.jp>
2024-11-05 14:02:57 +0900

    Add Camera Manager script
    Positions the action camera during shoot action

---
+++ Assets/Scripts/CameraManager.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class CameraManager : MonoBehaviour
+{
+    [SerializeField] private GameObject actionCameraGameObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        BaseAction.OnAnyActionStarted += BaseAction_OnAnyActionStarted;
+        BaseAction.OnAnyActionCompleted += BaseAction_OnAnyActionCompleted;
+
+        HideCamera();
+    }
+
+    private void BaseAction_OnAnyActionStarted(object sender, EventArgs e)
+    {
+        switch (sender)
+        {
+            case ShootAction shootAction:
+                Unit targetUnit = shootAction.GetTargetUnit();
+                Unit shootingUnit = shootAction.GetUnit();
+
+                Vector3 cameraCharacterHeight = Vector3.up * 1.7f;
+                
+                Vector3 shootDirection = (targetUnit.GetWorldPosition() - shootingUnit.GetWorldPosition()).normalized;
+
+                float shoulderOffset = 0.5f;
+                Vector3 shoulderOffsetPosition = Quaternion.Euler(0f, 90f, 0f) * shootDirection * shoulderOffset;
+
+                Vector3 shootingPosition = shootingUnit.GetWorldPosition() +
+                                           cameraCharacterHeight +
+                                           shoulderOffsetPosition +
+                                           shootDirection * (-1);
+
+                actionCameraGameObject.transform.position = shootingPosition;
+                actionCameraGameObject.transform.forward = shootDirection;
+
+                ShowCamera();
+
+                break;
+        }
+    }
+
+    private void BaseAction_OnAnyActionCompleted(object sender, EventArgs e)
+    {
+        switch (sender)
+        {
+            case ShootAction shootAction:
+                HideCamera();
+                break;
+        }
+    }
+
+    private void ShowCamera()
+    {
+        actionCameraGameObject.SetActive(true);
+    }
+
+    private void HideCamera()
+    {
+        actionCameraGameObject.SetActive(false);
+    }
+}
