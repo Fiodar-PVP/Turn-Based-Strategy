e954ffd1f2097ea6ccac4d25078fe4cca02fa51e
Fiodaru <f.perakhozhau@pvp.co.jp>
2024-11-06 15:06:55 +0900

    Implement basic EnemyAI
    During Enemy Turn all enemies will attempt to perform spin action. If no more enemies can take an action, end enemy turn

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -1,8 +1,22 @@
+using System;
 using UnityEngine;
 
 public class EnemyAI : MonoBehaviour
 {
+    private enum State
+    {
+        WaitingForEnemyTurn,
+        TakingTurn,
+        Busy
+    }
+
+    private State state;
     private float timer;
+
+    private void Awake()
+    {
+        state = State.WaitingForEnemyTurn;
+    }
 
     private void Start()
     {
@@ -11,7 +25,12 @@ public class EnemyAI : MonoBehaviour
 
     private void TurnSystem_OnTurnChanged(object sender, System.EventArgs e)
     {
-        timer = 2f;
+        if (!TurnSystem.Instance.IsPlayerTurn())
+        {
+            timer = 2f;
+
+            state = State.TakingTurn;
+        }
     }
 
     private void Update()
@@ -21,10 +40,69 @@ public class EnemyAI : MonoBehaviour
             return;
         }
 
-        timer -= Time.deltaTime;
-        if(timer <= 0) 
+        switch (state)
         {
-            TurnSystem.Instance.NextTurn();
+            case State.WaitingForEnemyTurn:
+                break;
+            case State.TakingTurn:
+                timer -= Time.deltaTime;
+                if (timer <= 0)
+                {
+                    if (TryTakeEnemyAIAction(SetStateTakingTurn))
+                    {
+                        state = State.Busy;
+                    }
+                    else
+                    {
+                        // No more enemies have actions they can take, end enemy turn
+                        state = State.WaitingForEnemyTurn;
+                        TurnSystem.Instance.NextTurn();
+                    }
+                }
+                break;
+            case State.Busy: 
+                break;
         }
+    }
+
+    private bool TryTakeEnemyAIAction(Action OnEnemyAIActionCompleted)
+    {
+        foreach (Unit enemyUnit in UnitManager.Instance.GetEnemyUnitList()) 
+        {
+            if (TryTakeEnemyAIAction(enemyUnit, OnEnemyAIActionCompleted))
+            {
+                return true;       
+            }
+        }
+
+        return false;
+    }
+
+    private bool TryTakeEnemyAIAction(Unit enemyUnit, Action OnEnemyAIActionCompleted)
+    {
+        SpinAction spinAction = enemyUnit.GetSpinAction();
+
+        GridPosition gridPosition = enemyUnit.GetGridPosition();
+
+        if (!spinAction.IsValidActionGridPosition(gridPosition))
+        {
+            return false;
+        }
+
+        if (!enemyUnit.TryToSpendActionPoints(spinAction))
+        {
+            return false;
+        }
+
+        spinAction.TakeAction(gridPosition, OnEnemyAIActionCompleted);
+
+        return true;
+    }
+
+    private void SetStateTakingTurn()
+    {
+        timer = 0.5f;
+
+        state = State.TakingTurn;
     }
 }
