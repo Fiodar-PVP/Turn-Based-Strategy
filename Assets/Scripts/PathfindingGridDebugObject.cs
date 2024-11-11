6f0b627ed1ed5a4d9a88a0290ff1f9374d676ba4
Fiodaru <f.perakhozhau@pvp.co.jp>
2024-11-11 17:33:15 +0900

    Create a Pathfinding Grid Debug Object
    This is an object which is instantiated in each grid position of the grid system in order to visualize data stored in the PathNode assosiated with that grid position. Script extends GridDebugObject, hence need to make function virtual to be able to override them

---
+++ Assets/Scripts/PathfindingGridDebugObject.cs
@@ -0,0 +1,27 @@
+using TMPro;
+using UnityEngine;
+
+public class PathfindingGridDebugObject : GridDebugObject
+{
+    [SerializeField] private TextMeshPro gCostText;
+    [SerializeField] private TextMeshPro hCostText;
+    [SerializeField] private TextMeshPro fCostText;
+
+    private PathNode pathNode;
+
+    protected override void Update()
+    {
+        base.Update();
+
+        gCostText.text = pathNode.GetGCost().ToString();
+        gCostText.text = pathNode.GetHCost().ToString();
+        gCostText.text = pathNode.GetFCost().ToString();
+    }
+
+    public override void SetGridObject(object gridObject)
+    {
+        base.SetGridObject(gridObject);
+
+        pathNode = (PathNode)gridObject;
+    }
+}
