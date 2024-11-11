4869324c54efc5611616bdd95dc812327234978f
Fiodaru <f.perakhozhau@pvp.co.jp>
2024-11-11 17:36:44 +0900

    Create a Pathfinding
    Create an instance of generic GridSystem class using PathNode class as parameter. Convert it to singleton.

---
+++ Assets/Scripts/Pathfinding.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class Pathfinding : MonoBehaviour
+{
+    public static Pathfinding Instance { get; private set; }
+
+    [SerializeField] private Transform gridDebugObjectPrefab;
+
+    private GridSystem<PathNode> gridSystem;
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Debug.Log("There is more than 1 Pathfinding system! " + transform + " - " + Instance);
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+
+    }
+
+    public void Setup(int width, int height, float cellSize)
+    {
+        gridSystem = new GridSystem<PathNode>(height, width, cellSize,
+            (GridSystem<PathNode> gridSystem, GridPosition gridPosition) => new PathNode(gridPosition));
+        gridSystem.CreateGridDebugObject(gridDebugObjectPrefab);
+    }
+}
+
