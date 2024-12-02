6221995f1166c40ff3f3f0bdc3e23fdc2eb3a794
Fiodaru <f.perakhozhau@pvp.co.jp>
2024-12-02 15:11:10 +0900

    Add DestructibleCrate Prefab

---
+++ Assets/Scripts/DestructibleCrate.cs
@@ -0,0 +1,22 @@
+using System;
+using UnityEngine;
+
+public class DestructibleCrate : MonoBehaviour
+{
+    public static event EventHandler OnAnyCrateDestruction;
+
+    private GridPosition gridPosition;
+
+    private void Start()
+    {
+        gridPosition = LevelGrid.Instance.GetGridPosition(transform.position);
+    }
+
+    public void Damage()
+    {
+        Destroy(gameObject);
+        OnAnyCrateDestruction?.Invoke(this, EventArgs.Empty);
+    }
+
+    public GridPosition GetGridPosition() => gridPosition;
+}
