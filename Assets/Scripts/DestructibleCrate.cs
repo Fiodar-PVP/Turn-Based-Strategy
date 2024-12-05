931eb25c820c9e65e751b74733b6439141b7c865
Fiodaru <f.perakhozhau@pvp.co.jp>
2024-12-05 10:46:56 +0900

    Update DestructibleCrate script
    Instantiate DestructedCrate Prefab and apply explosion force to it, once Crate is damaged/destroyed

--- Assets/Scripts/DestructibleCrate.cs
+++ Assets/Scripts/DestructibleCrate.cs
@@ -5,6 +5,8 @@ public class DestructibleCrate : MonoBehaviour
 {
     public static event EventHandler OnAnyCrateDestruction;
 
+    [SerializeField] private Transform destroyedCratePrefab;
+
     private GridPosition gridPosition;
 
     private void Start()
@@ -14,9 +16,28 @@ public class DestructibleCrate : MonoBehaviour
 
     public void Damage()
     {
+        Transform destroyedCrateTranform = Instantiate(destroyedCratePrefab, transform.position, transform.rotation);
+
+        float explosionForce = 150f;
+        float explosionRadius = 10f;
+        ApplyExplosionToRagdoll(destroyedCrateTranform, explosionForce, transform.position, explosionRadius);
+
         Destroy(gameObject);
         OnAnyCrateDestruction?.Invoke(this, EventArgs.Empty);
     }
 
     public GridPosition GetGridPosition() => gridPosition;
+
+    private void ApplyExplosionToRagdoll(Transform rootTransform, float explosionForce, Vector3 explosionPosition, float explosionRadius)
+    {
+        foreach (Transform child in rootTransform)
+        {
+            if (child.TryGetComponent(out Rigidbody rigidbody))
+            {
+                rigidbody.AddExplosionForce(explosionForce, explosionPosition, explosionRadius);
+            }
+
+            ApplyExplosionToRagdoll(child, explosionForce, explosionPosition, explosionRadius);
+        }
+    }
 }
