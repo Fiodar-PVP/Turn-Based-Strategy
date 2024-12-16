2b0edc5d2ebfcc063b40d4551f328a8de352a668
Fiodaru <f.perakhozhau@pvp.co.jp>
2024-12-16 17:47:21 +0900

    Adapt GrenadeProjectile to work with IDestructable interface

--- Assets/Scripts/GrenadeProjectile.cs
+++ Assets/Scripts/GrenadeProjectile.cs
@@ -40,9 +40,9 @@ public class GrenadeProjectile : MonoBehaviour
                     targetUnit.Damage(grenadeDamage);
                 }
 
-                if(collider.TryGetComponent<DestructibleCrate>(out DestructibleCrate destructibleCrate))
+                if(collider.TryGetComponent<IDestructable>(out IDestructable destructableObject))
                 {
-                    destructibleCrate.Damage();
+                    destructableObject.Destruction();
                 }
             }
 
