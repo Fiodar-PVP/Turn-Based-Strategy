86172c768f6d30c25565f87f7b8625ffc2c10e52
Fiodaru <f.perakhozhau@pvp.co.jp>
2024-12-16 15:57:03 +0900

    Implement Interact Action

---
+++ Assets/Scripts/Actions/InteractAction.cs
@@ -0,0 +1,87 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractAction : BaseAction
+{
+    private int maxInteractDistance = 1;
+
+    private void Update()
+    {
+        if (!isActive)
+        {
+            return;
+        }
+    }
+
+    public override string GetActionName()
+    {
+        return "Interact";
+    }
+
+    public override EnemyAIAction GetEnemyAIAction(GridPosition gridPosition)
+    {
+        return new EnemyAIAction
+        {
+            gridPosition = gridPosition,
+            actionValue = 0
+        };
+    }
+
+    public override List<GridPosition> GetValidActionGridPositionList()
+    {
+        List<GridPosition> validActionGridPositionList = new List<GridPosition>();
+        GridPosition unitGridPosition = unit.GetGridPosition();
+
+        for (int x = -maxInteractDistance; x <= maxInteractDistance; x++)
+        {
+            for (int z = -maxInteractDistance; z <= maxInteractDistance; z++)
+            {
+                GridPosition offsetGridPosition = new GridPosition(x, z);
+                GridPosition testGridPosition = offsetGridPosition + unitGridPosition;
+
+                if (!LevelGrid.Instance.IsValidGridPosition(testGridPosition))
+                {
+                    //Not valid GridPosition
+                    continue;
+                }
+
+                if (testGridPosition == unitGridPosition)
+                {
+                    //Unit is already located in that GridPosition
+                    continue;
+                }
+
+                if (LevelGrid.Instance.HasAnyUnitOnGridPosition(testGridPosition))
+                {
+                    //GridPosition is already occupied by another unit
+                    continue;
+                }
+
+                if (!LevelGrid.Instance.HasDoorAtGridPosition(testGridPosition))
+                {
+                    //No door at this gridPosition
+                    continue;
+                }
+
+                validActionGridPositionList.Add(testGridPosition);
+            }
+        }
+
+        return validActionGridPositionList;
+    }
+
+    public override void TakeAction(GridPosition gridPosition, Action OnActionComplete)
+    {
+        Door door = LevelGrid.Instance.GetDoorAtGridPosition(gridPosition);
+        door.Interact(OnInteractComplete);
+
+        ActionStart(OnActionComplete);
+    }
+
+    private void OnInteractComplete()
+    {
+        ActionComplete();
+    }
+}
